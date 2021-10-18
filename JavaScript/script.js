344ea40d80d2c0ade45b477889818d987ee9131b
Arun Kudiyal <arunkudiyal@icloud.com>
2021-10-18 12:23:05 +0530

    JavaScript - Day 1

---
+++ JavaScript/script.js
@@ -0,0 +1,38 @@
+// JavaScript is a progg lang, embed logic in your website / apps
+
+// ANYTHING & EVERYTHING IN JS ARE OBJECTS
+
+// Display Outputs in JS
+// console.log('Hello World...')
+// console.error('Error: Something went wrong!')
+// console.warn('Warning: This is just a warning!')
+
+// alert('Hey... this is a alert message!')
+
+
+// DATA TYPES
+
+// Variable Declaration
+// variableName = constantValue
+
+// x = 10
+// y = 'Innomatics Research Labs'
+// console.log(x)
+// console.log(y)
+
+// Data Types - number, string, boolean, null, undefined, symbol
+
+// Reserach Work - symbol data type
+
+a = 10.9999999
+b = 'Any string value'
+c = true
+variable = undefined
+variable2 = null
+
+console.log(typeof a)
+console.log(typeof b)
+console.log(typeof c)
+console.log(typeof variable2)       // object - An error from JS 
+
+console.log(typeof variable)
