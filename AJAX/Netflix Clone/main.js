183a2e329945123372a5039ebf26d69000fbfe57
Arun Kudiyal <arunkudiyal@icloud.com>
2021-11-12 11:50:50 +0530

    Netflix CLone - Images & Videos

--- AJAX/Netflix Clone/main.js
+++ AJAX/Netflix Clone/main.js
@@ -5,7 +5,7 @@ const data = null
 const btn = document.querySelector('#serach-btn')
 btn.addEventListener('click', () => {
     const serach = document.querySelector('#search').value
-
+    
     let url = `https://api.themoviedb.org/3/search/movie?api_key=473ce42b7b92f1fcd1bb5f06b4217fdb&query=`
 
     url = `${url}${serach}`
@@ -27,8 +27,22 @@ btn.addEventListener('click', () => {
             let output = ''
 
             for(let i=0; i < response.results.length; i++) {
+                const image = `<img src="https://image.tmdb.org/t/p/w500/${response.results[i].poster_path}" style="height: 300px; width: 300px;" />`
+
                 if(response.results[i].title != undefined) {
-                    output += `<b><u>${response.results[i].title}</u></b> <br /> <br /> ${response.results[i].overview} <br /> <br /> <hr />`
+                    output += `<b><u>${response.results[i].title} </u> <br /> <i> (Release : ${response.results[i].release_date}) </i> <br /> <br />  ${image} <br/ > <br /> </b> <br /> <br /> ${response.results[i].overview} <br /> <br /> <hr />`
+                }
+
+                let videoUrl = `https://api.themoviedb.org/3/movie/${response.results[i].id}?api_key=473ce42b7b92f1fcd1bb5f06b4217fdb&append_to_response=videos`
+
+                // console.log(videoUrl)
+
+                xhr.open('GET', videoUrl)
+
+                xhr.onreadystatechange = () => {
+                    if(xhr.status == 200 && xhr.readyState == 4) {
+                        console.log(xhr.responseText)
+                    }
                 }
             }
 
